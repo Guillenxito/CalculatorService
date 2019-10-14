549709474784e67e3cfad4555592d109b0923c53
Alvaro Guillen <aguillen@evicertia.com>
2019-10-14 15:47:48 +0200

    The log have been modified

---
+++ CalculatorS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CalculatorS
+{
+	public static class Program
+	{
+		public static string GetIdTracking(string id) {
+			if (id.Any())
+			{
+				return id;
+			}
+			else
+			{
+				return "NULL";
+			}
+		}
+	}
+}
